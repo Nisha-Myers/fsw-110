afe29ebd0d06f823c457b809d8b0f844c0488848
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-27 22:17:22 -0400

    .......

---
+++ week1/document_object/index.js
@@ -0,0 +1,6 @@
+function changeBodyBg(color){
+    document.body.style.background = color;
+} 
+function changeHeadingBg(color){
+document.getElementById("header").style.background = color;
+}
---
+++ week1/selecting_elements/index2.js
@@ -0,0 +1,4 @@
+var x = document.getElementById("header");
+var text = document.createTextNode("This was inserted with JavaScript");
+
+x.appendChild(text);
