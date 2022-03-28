8cec50e2a739f9b465bb5e6f3fa9534731924472
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-28 14:08:37 -0400

    ...

--- week1/selecting_elements/index2.js
+++ week1/selecting_elements/index2.js
@@ -1,34 +1,3 @@
-/*
-let element = document.getElementById('heading');
-element.innerHTML += "This was inserted with JavaScript";
-*/
-
-/*
-let h1Node = document.createElement('h1');
-hiNode.id = 'heading';
-hiNode.textContent = "This was inserted with JavaScript";
-document.body.appendChild(hiNode);
-*/
-
-/*
-function addText(text) {
-    document.getElementById('heading').innerHTML = "This was inserted with JavaScript"
-}
-*/
-/*
-var h = document.createElement("H1"); // Create the H1 element 
-var t = document.createTextNode("Your H1 text"); // Create a text element 
-h.appendChild(t); // Append the text node to the H1 element 
- 
-document.body.appendChild(h); // Append the H1 element to the document body 
-*/
-
-/*
-document.getElementById("heading").innerHTML = "This was inserted with JavaScript";
-*/
-
-//let textNode = document.createTextNode("Hello World"); document.body.appendChild(textNode);
-
 function newContent() {
     document.open();
     document.write("<h1>This text was inserted with JavaScript!</h1>");
