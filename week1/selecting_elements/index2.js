dde4055634e511f21a0e3c42629690fe2f0d4540
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-28 14:03:32 -0400

    ...

--- week1/selecting_elements/index2.js
+++ week1/selecting_elements/index2.js
@@ -31,6 +31,6 @@ document.getElementById("heading").innerHTML = "This was inserted with JavaScrip
 
 function newContent() {
     document.open();
-    document.write("<h1>Out with the old, in with the new!</h1>");
+    document.write("<h1>This text was inserted with JavaScript!</h1>");
     document.close();
   }
