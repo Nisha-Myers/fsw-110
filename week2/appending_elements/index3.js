e2c5396b6eedc8c74bf6f1ff26db87b0c5d88d33
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-30 18:16:51 -0400

    ....

--- week2/appending_elements/index3.js
+++ week2/appending_elements/index3.js
@@ -1,26 +1,26 @@
 var heading = document.createElement("h1");
 heading.textContent = "My FSW-110 Website";
-var header = document.getElementById("heading");
-header.appendChild(heading);
+var myHeader = document.getElementById("heading");
+myHeader.appendChild(heading);
 
 var myParagraph = document.createElement("p");
 myParagraph.textContent = "All of this was created with Javascript";
-var paragraph = document.getElementById("paragraph");
-paragraph.appendChild(myParagraph);
+var aParagraph = document.getElementById("paragraph");
+aParagraph.appendChild(myParagraph);
 
 
 
 var newItem = document.createElement("li");
 newItem.textContent = "drawing"
 var mylist = document.getElementById("one");
-mylist.prepend(newItem);
+mylist.append(newItem);
 
 var newItem = document.createElement("li");
 newItem.textContent = "sculpting"
 var mylist = document.getElementById("two");
-mylist.appendChild(newItem);
+mylist.append(newItem);
 
 var newItem = document.createElement("li");
 newItem.textContent = "gaming"
 var mylist = document.getElementById("three");
-mylist.appendChild(newItem);
+mylist.append(newItem);
