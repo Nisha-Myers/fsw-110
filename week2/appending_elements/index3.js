aacf66b27d1caa8370d760c745f902099b696277
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-30 17:58:23 -0400

    ...

--- week2/appending_elements/index3.js
+++ week2/appending_elements/index3.js
@@ -12,15 +12,15 @@ paragraph.appendChild(myParagraph);
 
 var newItem = document.createElement("li");
 newItem.textContent = "drawing"
-var mylist = document.getElementById("mylist");
+var mylist = document.getElementById("one");
 mylist.prepend(newItem);
 
 var newItem = document.createElement("li");
 newItem.textContent = "sculpting"
-var mylist = document.getElementById("mylist");
+var mylist = document.getElementById("two");
 mylist.appendChild(newItem);
 
 var newItem = document.createElement("li");
 newItem.textContent = "gaming"
-var mylist = document.getElementById("mylist");
+var mylist = document.getElementById("three");
 mylist.appendChild(newItem);
