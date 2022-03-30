632a5fa328df53747064709aac269c90b900673e
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-30 11:16:29 -0400

    ...

--- week1/selecting_elements/index2.js
+++ week1/selecting_elements/index2.js
@@ -1,6 +1,8 @@
 //document.getElementById("heading").innerHTML = "This text was inserted with JavaScript";
 
-var header = document.createElement("h1");
-header.textContent = "This text was inserted with JavaScript";
-var heading = document.getElementById("heading");
-heading.appendChild(header);
+
+let element = document.getElementById('heading');
+
+element.innerHTML += 'This text was inserted with JavaScript';
+
+
