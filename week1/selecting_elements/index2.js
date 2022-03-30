42945cd7251a1a0cea6680a9077aac7fc42fa0cc
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-03-30 15:26:57 -0400

    ....

--- week1/selecting_elements/index2.js
+++ week1/selecting_elements/index2.js
@@ -1,8 +1,12 @@
 //document.getElementById("heading").innerHTML = "This text was inserted with JavaScript";
 
-
+/*
 let element = document.getElementById('heading');
-
 element.innerHTML += 'This text was inserted with JavaScript';
+*/
 
+var heading = document.createElement("h1");
+heading.textContent = "Welcome to my JS site";
+var header = document.getElementById("mylist");
+header.appendChild(heading);
 
--- week2/appending_elements/index3.js
+++ week2/appending_elements/index3.js
@@ -1,7 +1,33 @@
+document.getElementById("heading").innerHTML = "Welcome to my JS site";
+
+document.getElementById("paragraph").innerHTML = "All of this was created with Javascript";
+
+var newItem = document.getElementById("heading");
+newItem.textContent = "Welcome to my JS site";
+
+var heading = document.createElement("h1");
+heading.textContent = "Welcome to my JS site";
+var header = document.getElementById("header");
+header.appendChild(heading);
+
+var myParagraph = document.createElement("p");
+myParagraph.textContent = "All of this was created with Javascript";
+var paragraph = document.getElementById("mylist");
+paragraph.appendChild(myParagraph);
+
+
+
+var newItem = document.createElement("li");
+newItem.textContent = "1"
+var mylist = document.getElementById("mylist");
+mylist.prepend(newItem);
+
+var newItem = document.createElement("li");
+newItem.textContent = "2"
+var mylist = document.getElementById("mylist");
+mylist.append(newItem);
+
 var newItem = document.createElement("li");
 newItem.textContent = "3"
 var mylist = document.getElementById("mylist");
 mylist.appendChild(newItem);
-
-//var myParagraph = document.createElement("p");
-
