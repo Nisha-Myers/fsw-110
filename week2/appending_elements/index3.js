a1deb1c517024a6336ecb8799f412a38fc32a6a1
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-04-03 21:22:01 -0400

    .

--- week2/appending_elements/index3.js
+++ week2/appending_elements/index3.js
@@ -1,41 +1,40 @@
-var heading = document.createElement("h1");
-heading.textContent = "Welcome to my JS site";
-var myHeader = document.getElementById("heading");
-myHeader.appendChild(heading);
+// my heading
+var newH = document.createElement("h1");
+newH.textContent = "Welcome to my JS site"
+document.body.appendChild(newH);
 
-var myParagraph = document.createElement("p");
-myParagraph.textContent = "All of this was created with Javascript";
-var aParagraph = document.getElementById("paragraph");
-aParagraph.appendChild(myParagraph);
+// my paragraph
+var newP = document.createElement("p");
+newP.textContent = "All of this was created with JavaScript!"
+document.body.appendChild(newP);
 
 
+// my ordered list
+var newList = document.createElement("ol");
+newList.textContent = "~My Hobbies~"
+document.body.appendChild(newList);
+
+// my 6 list items
+var newItem = document.createElement("li");
+newItem.appendChild(document.createTextNode("gaming"));
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "studying for my College classes"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("drawing"));
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "practicing coding"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("sculpting"));
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "playing guitar"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("playing guitar"));
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "sculpting"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("practicing coding"));
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "drawing"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
-
-var newItem = document.createElement("li");
-newItem.textContent = "gaming"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("studying for my College classes"));
+document.querySelector("ol").appendChild(newItem);
