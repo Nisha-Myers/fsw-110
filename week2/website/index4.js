7f99f443baaa0427a0be29d4c4d761c320d47036
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-04-03 23:00:31 -0400

    .

--- week2/website/index4.js
+++ week2/website/index4.js
@@ -1,41 +1,85 @@
-var heading = document.createElement("h1");
-heading.textContent = "My FSW-110 Website Project";
-var myHeader = document.getElementById("heading");
-myHeader.appendChild(heading);
-
-var myParagraph = document.createElement("p");
-myParagraph.textContent = "My favorite hobbies include:";
-var aParagraph = document.getElementById("paragraph");
-aParagraph.appendChild(myParagraph);
+// my navbar
+var newNav = document.createElement("nav");
+newNav.textContent = "home about news";
+document.body.prepend(newNav);
 
 
+
+/*
+if (window.location.href.contains('/home')) {
+    document.getElementsByClassName('Home-link')[0]?.classList.add('active');
+}
+*/
+
+
+
+
+
+// my heading
+var newH = document.createElement("h1");
+newH.textContent = "~This Is My FSW-110 Site~";
+newH.style.textAlign = "center";
+newH.style.fontSize = "60px";
+document.body.appendChild(newH);
+
+// my paragraph
+var newP = document.createElement("p");
+newP.textContent = "All of this was created with JavaScript!";
+newP.style.textAlign = "center";
+newP.style.fontSize = "25px";
+document.body.appendChild(newP);
+
+
+
+
+// my ordered list
+var newList = document.createElement("ol");
+newList.textContent = "~My Hobbies~"
+newList.style.fontSize = "30px";
+document.body.appendChild(newList);
+
+// my 6 list items
+var newItem = document.createElement("li");
+newItem.appendChild(document.createTextNode("gaming"));
+newItem.style.fontSize = "18px";
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "studying for my College classes"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("drawing"));
+newItem.style.fontSize = "18px";
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "practicing coding"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("sculpting"));
+newItem.style.fontSize = "18px";
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "playing guitar"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("playing guitar"));
+newItem.style.fontSize = "18px";
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "sculpting"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("practicing coding"));
+newItem.style.fontSize = "18px";
+document.querySelector("ol").appendChild(newItem);
 
 var newItem = document.createElement("li");
-newItem.textContent = "drawing"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+newItem.appendChild(document.createTextNode("studying for my College classes"));
+newItem.style.fontSize = "18px";
+document.querySelector("ol").appendChild(newItem);
 
-var newItem = document.createElement("li");
-newItem.textContent = "gaming"
-var mylist = document.getElementById("mylist");
-mylist.prepend(newItem);
+
+
+// my footer
+var newFooter = document.createElement("footer");
+newFooter.textContent = "Author: Denisha Shae Myers"
+newFooter.style.fontSize = "13px"
+document.body.appendChild(newFooter);
+
+
+// change background color
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ window.addEventListener("load",function() { changeBackground("lightcoral") });
