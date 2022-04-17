41a65fc2e2847125366c4839b046afa26ab464d6
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-04-16 22:12:51 -0400

    .

---
+++ week4/event-listeners/event.js
@@ -0,0 +1,65 @@
+
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("dblclick",function() { changeBackground("red") });
+
+ function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("scroll",function() { changeBackground("purple") });
+
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("load",function() { changeBackground("black") });
+
+ function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("mouseover",function() { changeBackground("green") });
+
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("click",function() { changeBackground("yellow") });
+
+ function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("change",function() { changeBackground("blue") });
+ 
+ function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ document.addEventListener("mouseout",function() { changeBackground("white") });
+ 
+
+ 
+ for (var i = 0; i < 25; i++) {      
+    var newH = document.createElement("h1");
+    newH.textContent = ".";
+    document.body.appendChild(newH);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ week4/module/mod.js
@@ -0,0 +1,8 @@
+var myMod = (function () {
+    var myMod = {
+      doSomething: () {
+        return "Hello everyone! How are you today?";
+      },
+    };
+    return myMod;
+  })();
