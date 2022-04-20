63e52ee1513aac04508937356ff0eced9e670ebe
Nisha.Myers <denisha.myers@bryanuniversity.edu>
2022-04-20 01:38:40 -0400

    fixed event project

--- week4/module/mod.js
+++ week4/module/mod.js
@@ -1,8 +1,5 @@
-var myMod = (function () {
-    var myMod = {
-      doSomething: () {
-        return "Hello everyone! How are you today?";
-      },
-    };
-    return myMod;
-  })();
+const getName = () => {
+  return 'Denisha';
+};
+
+exports.getName = getName;
